dca36119c242614cc89d31fc520e48349bf59441
Otávio Novais <104913831+Otavio-Novais@users.noreply.github.com>
2023-08-28 17:43:32 -0300

    Create cliente.ts

---
+++ cliente.ts
@@ -0,0 +1,16 @@
+export class Cliente
+{
+    nome: string
+    telefone: string
+    idx: number 
+
+    constructor(nome:string, telefone:string, idx: number)
+    {
+        this.nome = nome
+        this.telefone = telefone
+        this.idx = idx
+    }
+
+
+    
+}
