518cbd29cfe1413fb010abd87e924425c05a6e38
Otávio Novais <104913831+Otavio-Novais@users.noreply.github.com>
2023-09-09 21:28:39 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -7,6 +7,8 @@ const bike = new Bike('mountain bike', 'mountain',
     123, 500, 100.5, 'desc', 5, [])
 
 const user = new User('Maria', 'maria@mail.com', '1234')
+
+
 const today = new Date()
 const twoDaysFromToday = new Date()
 twoDaysFromToday.setDate(twoDaysFromToday.getDate() + 2)
@@ -14,20 +16,29 @@ const tomorrow = new Date()
 tomorrow.setDate(tomorrow.getDate() + 1)
 const sevenDaysFromToday = new Date()
 sevenDaysFromToday.setDate(sevenDaysFromToday.getDate() + 7)
+
 const rent1 = Rent.create([], bike, user, today, twoDaysFromToday)
 const user2 = new User('Maria Clara', 'maria@mail.com', '3123')
 
+
 const app = new App()
-app.registerUser(user)
-app.registerBike(bike)
-console.log(app.rents) 
-//console.log(app.users)
-//console.log(app.bikes)
+const idUser1 = app.registerUser(user) // Receiving User Id
+const idBike1 = app.registerBike(bike) // Receiving Bike Id
 
 const Id = app.RentBike(bike,user, today, sevenDaysFromToday)
-//console.log(Id)
-console.log(app.rents)
-app.returnBike(Id, user)
-console.log(app.rents)
+/*
+console.log(Id)
+*/
 
-//console.log(app.findUser('maria@mail.com'))
+/*
+ Listing bikes and Rents
+app.printBikes()
+app.printRents()
+app.printUsers()
+*/
+
+// Authenticated user
+app.authenticateUser(idUser1, '1234')
+
+// Unauthenticated user
+app.authenticateUser(idUser1, '12345')
