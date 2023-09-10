0d8e583550d22dc44e2b90e09cc7a1eb324bbaf3
Otávio Novais <104913831+Otavio-Novais@users.noreply.github.com>
2023-09-09 21:29:00 -0300

    Update user.ts

--- user.ts
+++ user.ts
@@ -1,8 +1,29 @@
-export class User {
-    constructor(
-        public name: string,
-        public email: string,
-        public password: string,
-        public id?: string
+import { Bike } from "./bike";
+import { User } from "./user";
+
+export class Rent
+ {
+    private constructor(
+        public bike: Bike,
+        public user: User,
+        public dateFrom: Date,
+        public dateTo: Date,
+        public dateReturned?: Date
     ) {}
-}
+
+    static create(rents: Rent[], bike: Bike, user: User, 
+                  startDate: Date, endDate: Date): Rent {
+        const canCreate = Rent.canRent(rents, startDate, endDate)
+        if (canCreate) return new Rent(bike, user, startDate, endDate)
+        throw new Error('Overlapping dates.')
+    }
+
+    static canRent(rents: Rent[], startDate: Date, endDate: Date): boolean {
+        for (const rent of rents) {
+            if (startDate <= rent.dateTo && endDate >= rent.dateFrom) {
+                return false
+            }
+        }
+        return true
+    }
+ }
