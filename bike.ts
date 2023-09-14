5786f8807c197cf895452bdca029dafa6d2b0436
Otávio Novais <104913831+Otavio-Novais@users.noreply.github.com>
2023-09-14 17:43:43 -0300

    Update bike.ts

--- bike.ts
+++ bike.ts
@@ -1,3 +1,5 @@
+import fetch from 'node-fetch';
+
 export class Bike
  {
     constructor(
@@ -10,5 +12,34 @@ export class Bike
         public ratings: number,
         public imageUrls: string[],
         public id?: string
-    ) {}
+    ) {}    
+
+    //Test
+    async Location(){
+        const url = 'https://api.ipdata.co?api-key=b60bc829eeef22f1fb67ee109e614d2a277389dc04563634e8c96348&&fields=city,region,country_name,continent_name,latitude,longitude';
+
+        try {
+            // Make the GET request
+            const response = await fetch(url);
+
+            // Check if the request was successful
+                if (!response.ok) {
+                    throw new Error('HTTP error ' + response.status);
+                }
+
+            // Parse the JSON from the response
+            const data = await response.json();
+
+                // Log the data
+            console.log(data);
+            
+            } catch (error) {
+                // Log any errors
+            console.log(error);
+            }
+        }
+
  }
+
+
+ 
